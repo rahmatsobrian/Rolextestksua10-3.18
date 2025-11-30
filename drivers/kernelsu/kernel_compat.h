3d0981412aca1ef431b56ce7f45fd88e8125baf6
Rahmat Sobrian <135594470+rahmatsobrian@users.noreply.github.com>
2025-11-30 12:01:28 +0700

    Fix include directives formatting in kernel_compat.h

--- drivers/kernelsu/kernel_compat.h
+++ drivers/kernelsu/kernel_compat.h
@@ -2,11 +2,11 @@
 #define __KSU_H_KERNEL_COMPAT
 
 #include <linux/uaccess.h>
-#include "linux/fs.h"
-#include "linux/key.h"
-#include "linux/version.h"
-#include "linux/key.h"
-#include "linux/cred.h"
+#include <linux/fs.h>
+#include <linux/key.h>
+#include <linux/version.h>
+#include <linux/key.h>
+#include <linux/cred.h>
 
 extern struct file *ksu_filp_open_compat(const char *filename, int flags,
 					 umode_t mode);
